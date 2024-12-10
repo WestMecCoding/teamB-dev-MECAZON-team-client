3981dc9aaedc8d7f371b523ae4764b139ac39661
Favian Perez <fperez593.westmec.org>
2024-12-10 14:27:36 -0700

    2nd redid

---
+++ src/components/Signup.jsx
@@ -0,0 +1,42 @@
+import styles from "../styles/Singup.module.css"
+import React, { useState } from 'react';
+
+export default function Signup() {
+
+    return (
+      <div>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    );
+
+}

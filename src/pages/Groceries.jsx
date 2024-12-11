fd47b726735428a6082a0ef9429638fe93f054d1
Micah Kinnard <mkinna608@west-mec.org>
2024-12-11 14:03:32 -0700

    12/11/2024 2:03PM micah's commit

--- src/pages/Groceries.jsx
+++ src/pages/Groceries.jsx
@@ -49,6 +49,11 @@ const handleCategoryFilter = (category) => {
       <select onChange={(e) => handleCategoryFilter(e.target.value)}>
         <option value="all">All Items</option>
         <option value="dairy">Dairy Products</option>
+        <option value="vegetables">Vegetable Products</option>
+        <option value="proteins">Protein Products</option>
+        <option value="fruits">Fruit Products</option>
+        <option value="nuts">Nut Products</option>
+        <option value="grains">Grain Products</option>
       </select>
       <h1>Groceries</h1>
       <SearchBar onSearch={handleSearch} />

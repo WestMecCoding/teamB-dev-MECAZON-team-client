b307b749eae3400ab254aafd995aeb78741d0165
rhatfi711 <142631246+rhatfi711@users.noreply.github.com>
2024-12-13 21:33:41 +0000

    end of code 12-13-24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styles from '../styles/footer.module.css';
+import fb from '../assets/fbimg.png';
+import x from '../assets/ximg.png';
+import linkedin from '../assets/linkedinimg.png';
+import insta from '../assets/instaimg.png';
+
 
 const Footer = () => {
     return (
@@ -29,9 +34,50 @@ const Footer = () => {
                         <p>STV</p>
                     </a>
                 </div>
+                <div className="sb__footer-links-div">
+                    <h4>Company</h4>
+                    <a href="/about">
+                        <p>About</p>
+                    </a>
+                    <a href="/press">
+                        <p>Press</p>
+                    </a>
+                    <a href="/career">
+                    <p>Career</p>
+                    </a>
+                    <a href="/contact">
+                    <p>Contact</p>
+                    </a>
+                </div>
+                <div className="sb__footer-links_div">
+                    <h4>Comming soon on</h4>
+                    <div className="socialmedia">
+                        <p><img src={fb} alt=""/></p>
+                        <p><img src={x} alt=""/></p>
+                        <p><img src={linkedin} alt=""/></p>
+                        <p><img src={insta} alt=""/></p>
+                    </div>
+                </div>
             </div>
+
+            <hr></hr>
+
+            <div className="sb__footer-below">
+                <div className="sb__footer-copyright">
+                    <p>
+                        @{new Date().getFullyear()} CodeInn. All rights reserved.
+                    </p>
+                </div>
+                <div className="sb_footer-below-links">
+                    <a href="/terms"><div><p>Terms & Conditions</p></div></a>
+                    <a href="/privacy"><div><p>Privacy</p></div></a>
+                    <a href="/security"><div><p>Security</p></div></a>
+                    <a href="/cookie"><div><p>Cookie Declaration</p></div></a>
+                </div>
             </div>
-        </div>
+
+                </div>
+            </div>
     )
 }
 

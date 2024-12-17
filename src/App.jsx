ea8b6463f87ccc5a946f9e914528daa76849eb45
Micah Kinnard <mkinna608@west-mec.org>
2024-12-17 13:42:46 -0700

    Micah fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Groceries from "./pages/Groceries";
-<<<<<<< HEAD
-import Cart from "./pages/Cart";
-
-import "./App.css";
-export default function App() {
-  return (
-      <Router>
-        <Navbar />
-=======
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import "./App.css";
@@ -19,20 +10,12 @@ export default function App() {
   return (
     <Router>
       <Navbar/>
->>>>>>> Rylan
       <Routes>
           <Route path="/" element={<Home />} />
         <Route path="/groceries" element={<Groceries />} />
-<<<<<<< HEAD
-        <Route path='/cart' element={<Cart/>}/>
-
-        </Routes>
-      </Router>
-=======
       </Routes>
       <Body/>
       <Footer/>
     </Router>
->>>>>>> Rylan
   );
 }
